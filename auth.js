f9c32d80a4759174bd51f7132a1548c2ebfb570c
dmiduncan <dmiduncan@gmail.com>
2025-12-10 21:42:59 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,5 @@
 // auth.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.86.0/+esm'
 // Replace with your own Supabase credentials
 const supabaseUrl = 'https://ljisujkxmbijleyhmxab.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqaXN1amt4bWJpamxleWhteGFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1MzMyOTYsImV4cCI6MjA3MDEwOTI5Nn0.9CbNfvI5VlUUQ4bbHd18pGR9ft-tHz2FLKAF_4yQJsg';
