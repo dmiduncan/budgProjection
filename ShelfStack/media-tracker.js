352c95ca7a20c6f37ccb7ea75683089174ba9e53
dmiduncan <dmiduncan@gmail.com>
2026-02-01 00:44:49 -0500

    Update media-tracker.js

--- ShelfStack/media-tracker.js
+++ ShelfStack/media-tracker.js
@@ -1030,7 +1030,7 @@ function displaySearchResults(results, searchTerm) {
                                 class="button"
                                 ${canQuickComplete ? 'style="display: none;"' : ''}
                                 onclick="quickComplete(${media.id})">
-                            Quick Complete
+                            Retro Finish
                         </button>
                     </div>
                 </div>
