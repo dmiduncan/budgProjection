4b545c3475162cefae29d4a70c1a230f5a383ca3
dmiduncan <dmiduncan@gmail.com>
2026-08-16 13:11:49 -0400

    Rename project in urls

--- ShelfStack/js/toolbar.js
+++ ShelfStack/js/toolbar.js
@@ -15,13 +15,13 @@ export function initToolbar() {
                 <span class="toolbar__logo">shelf<span>STACK</span></span>
             </div>
             <nav class="toolbar__nav" id="toolbar-nav">
-                <button type="button" class="toolbar__btn" title="Trends" onclick="window.location.href='https://dmiduncan.github.io/budgProjection/ShelfStack/trends/'">
+                <button type="button" class="toolbar__btn" title="Trends" onclick="window.location.href='https://dmiduncan.github.io/HandyHaversack/ShelfStack/trends/'">
                     <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24"
                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <polyline points="2,16 6,9 10,13 14,6 18,10 22,5"/>
                     </svg>
                 </button>
-                <button type="button" class="toolbar__btn" title="Budget Projection" onclick="window.location.href='https://dmiduncan.github.io/budgProjection/BudgProjection/'">
+                <button type="button" class="toolbar__btn" title="Budget Projection" onclick="window.location.href='https://dmiduncan.github.io/HandyHaversack/BudgProjection/'">
                     <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24"
                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <rect x="2" y="7" width="20" height="14" rx="2"/>
@@ -29,7 +29,7 @@ export function initToolbar() {
                         <path d="M22 11V7a2 2 0 0 0-2-2L6 5a2 2 0 0 1-2-2"/>
                     </svg>
                 </button>
-                <button type="button" class="toolbar__btn" id="toolbar-home-btn" title="Home" onclick="window.location.href='https://dmiduncan.github.io/budgProjection/ShelfStack/'">
+                <button type="button" class="toolbar__btn" id="toolbar-home-btn" title="Home" onclick="window.location.href='https://dmiduncan.github.io/HandyHaversack/ShelfStack/'">
                     <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none"
                          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M3 9.5L12 3l9 6.5V20a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.5z"/>
